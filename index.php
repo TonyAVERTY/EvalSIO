68d66bdb77e9e1cd626cbb815b5f2376ed66c8c8
Tony <contact@tony-averty.dev>
2025-12-04 14:29:54 +0100

    2nd consigne

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stud2Jobs</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav class="site-nav" aria-label="Navigation principale">
+                <ul>
+                    <li><a>Accueil</a></li>
+                    <li><a>Contactez-moi</a></li>
+                </ul>
+        </nav>
+    </header>
+</body>
+</html>
